0c3cfabd3467bea53058d252d710ea0405a65311
randomgitgen <randomgitgen99@outlook.com>
2025-06-03 14:27:34 +0200

    Signup V.1

--- PCW/PC/views.py
+++ PCW/PC/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 
 from PL.models import Product, Category
@@ -19,8 +19,17 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
+
 def signup(request):
-    form = SignupForm()
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect(" /login/")
+    else:
+        form = SignupForm()
 
     return render(request, "core/signup.html", {
         "form": form
