d9e5d7876b7d8347906bfd0397605d7ad4f74b74
Druby02 <danielruby002@gmail.com>
2025-06-03 12:46:52 +0200

    I dropped my hotdog on the ground

--- PCW/PCW/urls.py
+++ PCW/PCW/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
 from PC.views import contact, index
+
 urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),  
     path('admin/', admin.site.urls),
-]
+] 
